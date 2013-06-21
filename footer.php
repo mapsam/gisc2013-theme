3c4d0a8b09b163965bf1a9d1f925f0aaa1f6e953
Sam Matthews <matthews.sam@gmail.com>
2013-06-20 18:51:12 -0700

    remove rights reserved

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 </div>
 <footer>
 <div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'gisc' ), '&copy;', date('Y'), esc_html(get_bloginfo('name')) ); echo sprintf( __( ' Theme By: %1$s', 'gisc' ), '<a href="http://situatedlaboratories.com/">Situated Laboratories</a>' ); ?>
+<?php echo sprintf( __( '%1$s %2$s %3$s.', 'gisc' ), '&copy;', date('Y'), esc_html(get_bloginfo('name')) ); echo sprintf( __( ' Theme By: %1$s', 'gisc' ), '<a href="http://situatedlaboratories.com/">Situated Laboratories</a>' ); ?>
 </div>
 </footer>
 </div>
